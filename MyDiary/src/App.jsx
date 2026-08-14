928c7f41a80dfcfacdda28fd3aa1717741e17b6f
matsubokkuri <matsubokkuri00000@gmail.com>
2026-08-15 00:30:39 +0900

    ログイン機能実装

--- MyDiary/src/App.jsx
+++ MyDiary/src/App.jsx
@@ -1,12 +1,31 @@
 import DiaryForm from "./DiaryForm";
 import DiaryList from "./DiaryList";
+import { supabase } from "./supabase";
 import useDiaries from "./useDiaries";
 
 function App (){
   const { diaryList, addDiary, deleteDiary, upadateDiary } = useDiaries();
+
+  const handleSignUp = async ()=>{
+    
+    const { data, error } = await supabase.auth.signUp({
+      email: "...",
+      password: "..."
+    })
+
+    if(error){
+      console.log(error);
+      return;
+    }
+
+    console.log(data);
+  }
   
   return (
     <>
+      <button onClick={handleSignUp}>
+        ユーザ登録
+      </button>
       <h1>Diary</h1>
       <DiaryForm handleAddDiary={addDiary}/>
 
