30a1b9114195ec89f1408656bc29274a16d42bc5
matsubokkuri00000 <matsubokkuri00000@gmail.com>
2026-08-15 03:49:03 +0900

    ログイン機能，ログアウト機能実装

--- MyDiary/src/App.jsx
+++ MyDiary/src/App.jsx
@@ -1,9 +1,12 @@
+import { useState, useEffect } from "react";
 import DiaryForm from "./DiaryForm";
 import DiaryList from "./DiaryList";
 import { supabase } from "./supabase";
 import useDiaries from "./useDiaries";
 
 function App (){
+  const [user, setUser] = useState(null);
+
   const { diaryList, addDiary, deleteDiary, upadateDiary } = useDiaries();
 
   const handleSignUp = async ()=>{
@@ -21,11 +24,66 @@ function App (){
     console.log(data);
   }
   
+  const handleSignIn = async ()=>{
+    
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: "venulucifer20@gmail.com",
+      password: "lovelove235"
+    });
+
+    if(error){
+      console.log(error);
+      return;
+    }
+
+    console.log(data);
+    console.log(data.user);
+    console.log(data.user.id);
+  }
+
+  const handleSignOut = async ()=>{
+
+    const { error } = await supabase.auth.signOut();
+
+    if(error){
+      console.log(error);
+      return;
+    }
+
+    setUser(null);
+
+  }
+
+  useEffect(()=>{
+
+    const getCurrentUser = async ()=>{
+      const { data, error } = await supabase.auth.getUser();
+      
+      if(error){
+        console.log(error);
+        return;
+      }
+
+      setUser(data.user);
+    }
+
+    getCurrentUser();
+
+  },[]);
+
   return (
     <>
       <button onClick={handleSignUp}>
         ユーザ登録
       </button>
+      <button onClick={handleSignIn}>
+        ログイン
+      </button>
+      <button onClick={handleSignOut}>
+        ログアウト
+      </button>
+      <p>ログイン中のユーザ：{user?.email}</p>
+
       <h1>Diary</h1>
       <DiaryForm handleAddDiary={addDiary}/>
 
