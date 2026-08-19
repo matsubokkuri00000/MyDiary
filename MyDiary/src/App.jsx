3ca71a4951a4acb7e559084ea3e8380421993442
matsubokkuri00000 <matsubokkuri00000@gmail.com>
2026-08-20 03:31:32 +0900

    認証ガードとルーティング設計完了

--- MyDiary/src/App.jsx
+++ MyDiary/src/App.jsx
@@ -2,16 +2,46 @@ import { useContext } from "react";
 import Auth from "./Auth";
 import AuthContext from "./AuthContext";
 import DiaryPage from "./DiaryPage";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App (){
   const { user, loading, getCurrentUser, signUp, signIn, signOut } = useContext(AuthContext);
 
+  if(loading){
+    return <p>認証情報を確認中...</p>
+  }
+
   return (
     <>
-      {user && loading === false 
-        ? <DiaryPage />
-        : <Auth />
-      }
+      <Routes>
+        <Route 
+          path="/"
+          element={
+            user
+              ? <Navigate to="/diary" />
+              : <Navigate to="/login" />
+          }
+        />
+
+        <Route 
+          path="/login" 
+          element={
+            user
+              ? <Navigate to="/diary" />
+              : <Auth />
+          }
+        />
+
+        <Route 
+          path="/diary" 
+          element={
+            user
+             ? <DiaryPage />
+             : <Navigate to="/login" />
+          }
+        />
+
+      </Routes>  
     </>
   )
 }
--- MyDiary/src/main.jsx
+++ MyDiary/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import AuthContext, { AuthProvider } from './AuthContext.jsx'
+import { AuthProvider } from './AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-        <App />
-    </AuthProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
   </StrictMode>,
 )
