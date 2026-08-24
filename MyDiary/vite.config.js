99298e0287c547b8011441424b8e700c2a0eb9ce
matsubokkuri00000 <matsubokkuri00000@gmail.com>
2026-08-24 17:16:11 +0900

    vite-plugin-pwaを導入する

--- MyDiary/vite.config.js
+++ MyDiary/vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+
+        VitePWA({
+            registerType: "autoUpdate",
+
+            manifest: {
+                name: "MyDiary",
+                short_name: "MyDiary",
+                start_url: "/",
+                display: "standalone",
+
+                icons: [
+                    {
+                        src: "/icon_192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "/icon_512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ]
+            },
+
+            workbox: {
+              globPatterns: [
+                "**/*.{js, css, html, png, svg, ico}"
+              ]
+            }
+
+        })
+    ],
+});
