a771523553451d134bdb597d35ca206ad5505fde
matsubokkuri <matsubokkuri00000@gmail.com>
2026-09-06 15:55:38 +0900

    feat: カレンダー追加

--- MyDiary/src/pages/CalendarPage.jsx
+++ MyDiary/src/pages/CalendarPage.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+import { ja } from "date-fns/locale"
+import { DayPicker } from "@daypicker/react";
+import "@daypicker/react/style.css"
+import "../styles/calendar.css"
+
 const CalendarPage = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
     return (
-        <div>
-            <h2>Calendar</h2>
-            <p>カレンダーページ</p>
-        </div>
+        <section className="calendar-page">
+            <div className="calendar-layout">
+
+                <div className="calendar-left">
+                    <DayPicker 
+                        mode="single"
+                        selected={selectedDate}
+                        onSelect={setSelectedDate}
+                        
+                    />   
+                </div>
+
+                <div className="calendar-right">
+                    <p>
+                        選択中の日付：{selectedDate?.toLocaleDateString()}
+                    </p>  
+                    <p>
+                        ここに選択した日の投稿を表示
+                    </p>
+                </div>
+                
+            </div>       
+        </section>
     )
 }
 
