a63f0a30c71f95a6a17a704d7b105f5c7415c64e
matsubokkuri00000 <matsubokkuri00000@gmail.com>
2026-09-06 21:33:41 +0900

    feat: カレンダーページの基本UI実装

--- MyDiary/src/pages/CalendarPage.jsx
+++ MyDiary/src/pages/CalendarPage.jsx
@@ -12,11 +12,14 @@ const CalendarPage = () => {
             <div className="calendar-layout">
 
                 <div className="calendar-left">
+                    <h2 className="calendar-title">
+                        Calendar
+                    </h2>
                     <DayPicker 
                         mode="single"
                         selected={selectedDate}
                         onSelect={setSelectedDate}
-                        
+                        locale={ja}
                     />   
                 </div>
 
@@ -27,6 +30,29 @@ const CalendarPage = () => {
                     <p>
                         ここに選択した日の投稿を表示
                     </p>
+
+                        <p>ここに選択した日の投稿を表示</p>
+    <p>テスト日記1</p>
+    <p>テスト日記2</p>
+    <p>テスト日記3</p>
+    <p>テスト日記4</p>
+    <p>テスト日記5</p>
+    <p>テスト日記6</p>
+    <p>テスト日記7</p>
+    <p>テスト日記8</p>
+    <p>テスト日記9</p>
+    <p>テスト日記10</p>
+        <p>ここに選択した日の投稿を表示</p>
+    <p>テスト日記1</p>
+    <p>テスト日記2</p>
+    <p>テスト日記3</p>
+    <p>テスト日記4</p>
+    <p>テスト日記5</p>
+    <p>テスト日記6</p>
+    <p>テスト日記7</p>
+    <p>テスト日記8</p>
+    <p>テスト日記9</p>
+    <p>テスト日記10</p>
                 </div>
                 
             </div>       
