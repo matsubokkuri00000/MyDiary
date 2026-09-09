e2c86d177e029cc2294a7c6d21a0bb27db82da84
matsubokkuri <matsubokkuri00000@gmail.com>
2026-09-10 02:02:11 +0900

    refactor: 日記入力ページの一覧表示を削除

--- MyDiary/src/pages/DiaryPage.jsx
+++ MyDiary/src/pages/DiaryPage.jsx
@@ -1,11 +1,11 @@
 import useDiaries from "../hooks/useDiaries";
 import DiaryForm from "../components/diary/DiaryForm";
-import DiaryList from "../components/diary/DiaryList";
+//import DiaryList from "../components/diary/DiaryList";
 import Toast from "../components/toast/Toast";
 import Loading from "../components/loading/Loading.jsx";
 
 const DiaryPage = () => {
-    const {diaryList, deletingID, updatingID, fetchLoading, addLoading, addDiary, deleteDiary, updateDiary, successMessage, errorMessage} = useDiaries();
+    const {fetchLoading, addLoading, addDiary, successMessage, errorMessage} = useDiaries();
 
      return (
         <>
@@ -19,27 +19,10 @@ const DiaryPage = () => {
                     <Loading />
                 )
                 : (
-                    <>
-                        <DiaryForm 
-                            handleAddDiary={addDiary}
-                            addLoading={addLoading}
-                        />
-                        
-                        {diaryList.length > 0 
-                            ? (
-                                <DiaryList 
-                                    diaryList={diaryList}
-                                    handleDeleteDiary={deleteDiary}
-                                    handleupdateDiary={updateDiary}
-                                    updatingID={updatingID}
-                                    deletingID={deletingID}
-                                />
-                            ) 
-                            : (
-                                <p>保存された日記はありません</p>
-                            ) 
-                        }
-                    </>
+                    <DiaryForm 
+                        handleAddDiary={addDiary}
+                        addLoading={addLoading}
+                    />
                 )
             }
         </>
